b7739eb7cfabcf40ec5af24312a13e1d6a44be2b
kandlly <kandlly10000@gmail.com>
2018-05-19 08:43:17 +0900

    add java8 class

---
+++ src/main/java/java8/com/lambda.java
@@ -0,0 +1,12 @@
+package java8.com;
+
+public class lambda {
+	//Arrays.asList("a","b","c").forEach(e -> System.out.print(e));
+
+	public void greet(){
+		System.out.println("Print something.");
+	}
+
+
+
+}
